8416eea3daa8ae6213d777ab7a2b83c0b68db1db
Pritish Budhiraja - Juspay <pritish.budhiraja@juspay.in>
2023-11-28 16:31:37 +0530

    bugfix: pre-push - pre-push hook removed

--- public/hyperswitch/wasm/euclid_bg.wasm.d.ts
+++ public/hyperswitch/wasm/euclid_bg.wasm.d.ts
@@ -2,7 +2,12 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function setForexData(a: number, b: number): void;
-export function convertCurrency(a: number, b: number, c: number, d: number): void;
+export function convertCurrency(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): void;
 export function seedKnowledgeGraph(a: number, b: number): void;
 export function getValidConnectorsForRule(a: number, b: number): void;
 export function analyzeProgram(a: number, b: number): void;
@@ -22,7 +27,12 @@ export function getDescriptionCategory(a: number, b: number, c: number): void;
 export function parse(a: number, b: number, c: number): void;
 export function parseToString(a: number, b: number, c: number): void;
 export function __wbindgen_export_0(a: number, b: number): number;
-export function __wbindgen_export_1(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_export_1(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): number;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_export_2(a: number, b: number, c: number): void;
 export function __wbindgen_export_3(a: number): void;
