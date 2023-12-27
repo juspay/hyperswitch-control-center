6f43d4de71a69fb08d518ebe640bf85d3a9ca723
Riddhiagrawal001 <50551695+Riddhiagrawal001@users.noreply.github.com>
2023-12-27 12:18:53 +0530

    chore: independent files addition for paypal (#118)
    Co-authored-by: Pritish Budhiraja <77892330+PritishBudhiraja@users.noreply.github.com>

--- public/hyperswitch/module.js
+++ public/hyperswitch/module.js
@@ -110,3 +110,18 @@ function getVariantValues(str) {
     return [];
   }
 }
+
+function payPalCreateAccountWindow() {
+  (function (d, s, id) {
+    var js,
+      ref = d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+      js = d.createElement(s);
+      js.id = id;
+      js.async = true;
+      js.src =
+        "https://www.paypal.com/webapps/merchantboarding/js/lib/lightbox/partner.js";
+      ref.parentNode.insertBefore(js, ref);
+    }
+  })(document, "script", "paypal-js");
+}
--- src/screens/HyperSwitch/APIUtils/APIUtilsTypes.res
+++ src/screens/HyperSwitch/APIUtils/APIUtilsTypes.res
@@ -30,6 +30,7 @@ type entityName =
   | PAYMENT_REPORT
   | REFUND_REPORT
   | DISPUTE_REPORT
+  | PAYPAL_ONBOARDING
   | SURCHARGE
 
 type userRoleTypes = USER_LIST | ROLE_LIST | ROLE_ID | NONE
