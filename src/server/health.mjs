681f1ad69682c61d0ae55fa63b7bde46d5603d20
Jeeva Ramachandran <jeeva.ramachandran@juspay.in>
2024-02-08 15:57:40 +0530

    fix: proxy api

--- src/server/health.mjs
+++ src/server/health.mjs
@@ -1,34 +1,20 @@
 import * as Fs from "fs";
-import fetch from "node-fetch";
+
+// import fetch from "node-fetch";
+const request = require("request");
+
 const https = require("https");
+const http = require("http");
+
 const errorHandler = (res, result) => {
   res.writeHead(500, { "Content-Type": "application/json" });
   res.write(JSON.stringify(result));
   res.end();
 };
 
-let checkHealth = async (res) => {
-  let output = {
-    env_config: false,
-    app_file: false,
-    wasm_file: false,
-    api_health: false,
-  };
-  try {
-    let indexFile = "dist/hyperswitch/index.html";
+let doProxy = () => {
+  return new Promise((resolve, reject) => {
     let configFile = "dist/hyperswitch/env-config.js";
-    let data = Fs.readFileSync(indexFile, { encoding: "utf8" });
-    if (data.includes(`<script src="/env-config.js"></script>`)) {
-      output.env_config = true;
-    }
-    if (data.includes(`<div id="app"></div>`)) {
-      output.app_file = true;
-    }
-    if (
-      data.includes(`<script type="module" src="/wasm/euclid.js"></script>`)
-    ) {
-      output.wasm_file = true;
-    }
     let envString = Fs.readFileSync(configFile, { encoding: "utf8" });
     console.log(envString, "envString");
     const match = envString.match(/apiBaseUrl:\s*"([^"]*)"/);
@@ -49,27 +35,73 @@ let checkHealth = async (res) => {
 
     const proxyHost = routerProxyUrl;
     const proxyPort = routerProxyPort;
-
-    // Create a new http.Agent with custom proxy host and port
-    const customAgent = new https.Agent({
-      keepAlive: true,
-      keepAliveMsecs: 1000,
-      maxSockets: 10,
-      timeout: 60000,
-      proxy: {
-        host: proxyHost,
-        port: proxyPort,
+    // http.request({
+    //   host: proxyHost, // IP address of proxy server
+    //   port: proxyPort, // port of proxy server
+    //   method: 'CONNECT',
+    //   path: `${proxyHost}:${proxyPort}`,
+    // }).on('connect', (res, socket) => {
+    //   if (res.statusCode === 200) { // connected to proxy server
+    //     https.get({
+    //       host: apiBaseUrl,
+    //       socket: socket,    // using a tunnel
+    //       agent: false,      // cannot use a default agent
+    //       path: '/health'  // specify path to get from server
+    //     }, (res) => {
+    //       let chunks = []
+    //       res.on('data', chunk => chunks.push(chunk))
+    //       res.on('end', () => {
+    //         console.log('DONE', Buffer.concat(chunks).toString('utf8'))
+    //         resolve(Buffer.concat(chunks).toString('utf8'))
+    //       })
+    //     })
+    //   }
+    // }).on('error', (err) => {
+    //   console.log(err)
+    //   reject(err)
+    // }).end()
+    request(
+      {
+        url: `${apiBaseUrl}/health`,
+        method: "GET",
+        proxy: `http://${proxyHost}:${proxyPort}`,
       },
-    });
+      (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          console.log(body);
+          resolve(body);
+        } else {
+          reject(error);
+        }
+      },
+    );
+  });
+};
 
-    let helathApiResponse = await fetch(`${apiBaseUrl}/health`, {
-      method: "GET",
-      agent: customAgent,
-    });
-    if (helathApiResponse.ok) {
-      output.api_health = true;
+let checkHealth = async (res) => {
+  let output = {
+    env_config: false,
+    app_file: false,
+    wasm_file: false,
+    api_health: false,
+  };
+  try {
+    let indexFile = "dist/hyperswitch/index.html";
+
+    let data = Fs.readFileSync(indexFile, { encoding: "utf8" });
+    if (data.includes(`<script src="/env-config.js"></script>`)) {
+      output.env_config = true;
     }
-    console.log(helathApiResponse, "helathApiResponse");
+    if (data.includes(`<div id="app"></div>`)) {
+      output.app_file = true;
+    }
+    if (
+      data.includes(`<script type="module" src="/wasm/euclid.js"></script>`)
+    ) {
+      output.wasm_file = true;
+    }
+    let res = await doProxy();
+
     let values = Object.values(output);
     if (values.includes(false)) {
       throw "Server Error";
