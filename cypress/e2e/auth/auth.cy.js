a4791419026f14a4eb0676503f22135a189528ab
Jeeva Ramachandran <jeeva.ramachandran@juspay.in>
2024-02-09 18:47:24 +0530

    chore: add signup cypress test

--- cypress/e2e/auth/auth.cy.js
+++ cypress/e2e/auth/auth.cy.js
@@ -2,20 +2,33 @@ describe("Login Module", () => {
   beforeEach(() => {
     cy.visit("http://localhost:9000/");
   });
-  it("check the components in the login page", () => {
-    cy.url().should("include", "/login");
-    cy.get("#card-header").should("contain", "Hey there, Welcome back!");
-    cy.get("#card-subtitle").should("contain", "Sign up");
-    cy.get("#auth-submit-btn").should("exist");
-    cy.get("#tc-text").should("exist");
-    cy.get("#footer").should("exist");
-  });
 
   it("check the components in the sign up page", () => {
     cy.get("#card-subtitle").click();
     cy.url().should("include", "/register");
     cy.get("#card-header").should("contain", "Welcome to Hyperswitch");
     cy.get("#card-subtitle").should("contain", "Sign in");
+    cy.get("#auth-submit-btn").should("exist");
+    cy.get("#tc-text").should("exist");
+    cy.get("#footer").should("exist");
+  });
+
+  it("should successfully singup in with valid credentials", () => {
+    const username = "test@gmail.com";
+    const password = "test";
+    cy.get("[data-testid=email]").type(username);
+    cy.get("[data-testid=password]").type(password);
+    cy.get('button[type="submit"]').click({ force: true });
+    cy.url().should("eq", "http://localhost:9000/home");
+    cy.contains(
+      "Welcome to the home of your Payments Control Centre. It aims at providing your team with a 360-degree view of payments.",
+    ).should("be.visible");
+  });
+
+  it("check the components in the login page", () => {
+    cy.url().should("include", "/login");
+    cy.get("#card-header").should("contain", "Hey there, Welcome back!");
+    cy.get("#card-subtitle").should("contain", "Sign up");
     cy.get("#auth-submit-btn").should("exist");
     cy.get("#tc-text").should("exist");
     cy.get("#footer").should("exist");
