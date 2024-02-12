571ec506a6588d120e11195ac84fae2c3e5916cd
Jeeva Ramachandran <jeeva.ramachandran@juspay.in>
2024-02-12 12:18:46 +0530

    chore: add signup cypress test

--- cypress/e2e/auth/auth.cy.js
+++ cypress/e2e/auth/auth.cy.js
@@ -5,13 +5,34 @@ describe("Login Module", () => {
   });
 
   it("check the components in the sign up page", () => {
-    cy.get("#card-subtitle").click();
-    cy.url().should("include", "/register");
-    cy.get("#card-header").should("contain", "Welcome to Hyperswitch");
-    cy.get("#card-subtitle").should("contain", "Sign in");
-    cy.get("#auth-submit-btn").should("exist");
-    cy.get("#tc-text").should("exist");
-    cy.get("#footer").should("exist");
+    // cy.get("#card-subtitle").click();
+    // cy.url().should("include", "/register");
+    // cy.get("#card-header").should("contain", "Welcome to Hyperswitch");
+    // cy.get("#card-subtitle").should("contain", "Sign in");
+    // cy.get("#auth-submit-btn").should("exist");
+    // cy.get("#tc-text").should("exist");
+    // cy.get("#footer").should("exist");
+    const username = "test@gmail.com";
+    const password = "test12#";
+    // /user/signin
+    cy.request({
+      method: "POST",
+      url: `http://localhost:8080/user/signup`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: { email: username, password: password, country: "IN" },
+    })
+      .then((response) => {
+        console.log(response);
+        expect(response.status).to.be.within(200, 299);
+      })
+      .should((response) => {
+        // Check if there was an error in the response
+        if (response.status >= 400) {
+          throw new Error(`Request failed with status: ${response.status}`);
+        }
+      });
   });
 
   // it("should successfully singup in with valid credentials", () => {
@@ -35,18 +56,18 @@ describe("Login Module", () => {
   //   cy.get("#footer").should("exist");
   // });
 
-  // it("should successfully log in with valid credentials", () => {
-  //   const username = "test@gmail.com";
-  //   const password = "test";
-  //   cy.visit("http://localhost:9000/");
-  //   cy.get("[data-testid=email]").type(username);
-  //   cy.get("[data-testid=password]").type(password);
-  //   cy.get('button[type="submit"]').click({ force: true });
-  //   cy.url().should("eq", "http://localhost:9000/home");
-  //   cy.contains(
-  //     "Welcome to the home of your Payments Control Centre. It aims at providing your team with a 360-degree view of payments.",
-  //   ).should("be.visible");
-  // });
+  it("should successfully log in with valid credentials", () => {
+    const username = "test@gmail.com";
+    const password = "test12#";
+    cy.visit("http://localhost:9000/");
+    cy.get("[data-testid=email]").type(username);
+    cy.get("[data-testid=password]").type(password);
+    cy.get('button[type="submit"]').click({ force: true });
+    cy.url().should("eq", "http://localhost:9000/home");
+    cy.contains(
+      "Welcome to the home of your Payments Control Centre. It aims at providing your team with a 360-degree view of payments.",
+    ).should("be.visible");
+  });
 
   // it("should display an error message with invalid credentials", () => {
   //   cy.visit("http://localhost:9000/");
