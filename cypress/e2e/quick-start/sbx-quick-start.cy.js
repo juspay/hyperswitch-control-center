093c85c670429c7e6bc605bd1472281aa72032fe
Jeeva Ramachandran <jeeva.ramachandran@juspay.in>
2024-03-26 17:15:17 +0530

    chore: check test

--- cypress/e2e/quick-start/sbx-quick-start.cy.js
+++ cypress/e2e/quick-start/sbx-quick-start.cy.js
@@ -1,4 +1,4 @@
-let username = `cypressquickstart+${Math.round(+new Date() / 1000)}@gmail.com`;
+let username = `cypresssbxquickstart+${Math.round(+new Date() / 1000)}@gmail.com`;
 before(() => {
   cy.singup_curl(username, "cypress98#");
 });
