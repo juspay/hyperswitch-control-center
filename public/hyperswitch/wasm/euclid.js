a4e216c94c774f7e6f6d4d3b9f26e418337cb115
Jeeva Ramachandran <120017870+JeevaRamu0104@users.noreply.github.com>
2024-07-12 17:23:30 +0530

    chore: update latest wasm (#985)

--- public/hyperswitch/wasm/euclid.js
+++ public/hyperswitch/wasm/euclid.js
@@ -755,6 +755,10 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
         takeObject(arg0);
     };
+    imports.wbg.__wbg_new_72fb9a18b5ae2624 = function() {
+        const ret = new Object();
+        return addHeapObject(ret);
+    };
     imports.wbg.__wbg_set_8417257aaedc936b = function(arg0, arg1, arg2) {
         const ret = getObject(arg0).set(getObject(arg1), getObject(arg2));
         return addHeapObject(ret);
@@ -769,10 +773,6 @@ function __wbg_get_imports() {
     };
     imports.wbg.__wbg_set_f975102236d3c502 = function(arg0, arg1, arg2) {
         getObject(arg0)[takeObject(arg1)] = takeObject(arg2);
-    };
-    imports.wbg.__wbg_new_72fb9a18b5ae2624 = function() {
-        const ret = new Object();
-        return addHeapObject(ret);
     };
     imports.wbg.__wbg_new_16b304a2cfa7ff4a = function() {
         const ret = new Array();
