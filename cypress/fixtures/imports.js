6c456d890d2d0b28dbb15fb335022b521dff259b
Luis C. Rojano <lcrojano@gmail.com>
2024-10-14 14:18:42 -0500

    chore: add payment body fixture

--- cypress/fixtures/imports.js
+++ cypress/fixtures/imports.js
@@ -1,2 +1,4 @@
 import createConnectorBody from "./create-connector-body.json";
+import makePaymentBody from "./make-payment-body.json";
 export { createConnectorBody };
+export { makePaymentBody };
