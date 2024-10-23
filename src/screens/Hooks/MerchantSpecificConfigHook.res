9d05dadb846bf01e7af89c13afac8179af43fea6
Jeeva Ramachandran <jeeva.ramachandran@juspay.in>
2024-10-23 14:40:40 +0530

    chore: merchant specific config

---
+++ src/screens/Hooks/MerchantSpecificConfigHook.res
@@ -0,0 +1,59 @@
+/**
+ * @module MerchantSpecificConfigHook
+ *
+ * @description This exposes a hook to fetch the merchant specific config
+ *               and to check if the merchant has access to config
+ *
+ *  @functions
+ *  - fetchMerchantSpecificConfig : fetches the list of user group level access
+ *  - useHasEnabledForMerchant: checks if the merchant has access
+ *         @params
+ *         - config : merchant config
+ *
+ *
+ *
+ */
+type useMerchantSpecificConfig = {
+  fetchMerchantSpecificConfig: unit => promise<unit>,
+  useHasEnabledForMerchant: FeatureFlagUtils.config => bool,
+  merchantSpecificConfig: FeatureFlagUtils.merchantSpecificConfig,
+}
+
+let useMerchantSpecificConfig = () => {
+  open APIUtils
+  let updateAPIHook = useUpdateMethod(~showErrorToast=false)
+  let setMerchantSpecificConfig =
+    HyperswitchAtom.merchantSpecificConfigAtom->Recoil.useSetRecoilState
+  let {userInfo: {orgId, merchantId, profileId}} = React.useContext(UserInfoProvider.defaultContext)
+  let merchantSpecificConfig =
+    HyperswitchAtom.merchantSpecificConfigAtom->Recoil.useRecoilValueFromAtom
+  let fetchMerchantSpecificConfig = async () => {
+    try {
+      let domain = HyperSwitchEntryUtils.getSessionData(~key="domain", ~defaultValue="default")
+      let merchantConfigURL = ` ${GlobalVars.getHostUrlWithBasePath}/config/merchant?domain=${domain}`
+      let body =
+        [
+          ("org_id", orgId->JSON.Encode.string),
+          ("merchant_id", merchantId->JSON.Encode.string),
+          ("profile_id", profileId->JSON.Encode.string),
+        ]
+        ->Dict.fromArray
+        ->JSON.Encode.object
+      let response = await updateAPIHook(merchantConfigURL, body, Post)
+      let mapMerchantSpecificConfig = response->FeatureFlagUtils.merchantSpecificConfig
+      setMerchantSpecificConfig(_ => mapMerchantSpecificConfig)
+    } catch {
+    | Exn.Error(e) => {
+        let err = Exn.message(e)->Option.getOr("Failed to Fetch!")
+        Exn.raiseError(err)
+      }
+    }
+  }
+  let useHasEnabledForMerchant = (config: FeatureFlagUtils.config) => {
+    config.orgIds->Array.length > 0 ||
+    config.merchantIds->Array.length > 0 ||
+    config.profileIds->Array.length > 0
+  }
+
+  {fetchMerchantSpecificConfig, useHasEnabledForMerchant, merchantSpecificConfig}
+}
--- src/server/config.mjs
+++ src/server/config.mjs
@@ -23,6 +23,70 @@ function updateConfigWithEnv(updatedConfig, domain = "", prefix = "") {
   return updatedConfig;
 }
 
+/* 
+@module checkEnvValues
+
+Verify the presence of critical merchant specific config.
+ 
+*/
+
+function checkEnvValues(env, tomlConfig) {
+  if (env && env !== null && typeof env === "string") {
+    let data = env.split(",");
+    if (Array.isArray(data)) {
+      return data;
+    }
+  } else if (
+    tomlConfig &&
+    tomlConfig !== null &&
+    typeof tomlConfig === "object"
+  ) {
+    return tomlConfig;
+  } else {
+    return [];
+  }
+}
+
+/* 
+@module checkEnvValues
+
+Returns a configuration object populated with their values.
+ 
+*/
+
+function updateMerchantConfigWithEnv(tomlConfig, body, domain = "default") {
+  try {
+    let modifiedObj = {};
+    for (const key in tomlConfig) {
+      const envOrgIds =
+        process.env[`${domain}__merchant_config__${key}__org_ids`];
+      const envMetchantIds =
+        process.env[`${domain}__merchant_config__${key}__merchant_ids`];
+      const envProfileIds =
+        process.env[`${domain}__merchant_config__${key}__profile_ids`];
+      let orgIds = checkEnvValues(envOrgIds, tomlConfig[key]["org_ids"]).filter(
+        (id) => body.org_id == id,
+      );
+      let merchantIds = checkEnvValues(
+        envMetchantIds,
+        tomlConfig[key]["merchant_ids"],
+      ).filter((id) => body.org_id == id);
+      let profileIds = checkEnvValues(
+        envProfileIds,
+        tomlConfig[key]["profile_ids"],
+      ).filter((id) => body.org_id == id);
+      modifiedObj[key] = {
+        org_ids: orgIds,
+        merchant_ids: merchantIds,
+        profile_ids: profileIds,
+      };
+    }
+    return modifiedObj;
+  } catch (err) {
+    console.error(err, "Error in checking merchant specific config");
+    return err;
+  }
+}
 const errorHandler = (res, errorMessage = "something went wrong") => {
   res.writeHead(500, { "Content-Type": "text/plain" });
   console.log(errorMessage);
@@ -31,7 +95,7 @@ const errorHandler = (res, errorMessage = "something went wrong") => {
 
 // Main function to read TOML file, override config, and output the result
 const configHandler = (
-  _req,
+  req,
   res,
   is_deployed = false,
   domain = "default",
@@ -69,7 +133,7 @@ const configHandler = (
             "theme",
           );
         } else {
-          merchantConfig = updateConfigWithEnv("default", "", merchantConfig);
+          merchantConfig = updateConfigWithEnv(merchantConfig, "default", "");
         }
       } catch {
         errorHandler(res, "Error on Overding ENV");
@@ -92,4 +156,95 @@ const configHandler = (
   }
 };
 
-export { configHandler };
+/* 
+@module getRequestBody
+
+To Parse the request body
+ 
+*/
+
+const getRequestBody = (req) => {
+  return new Promise((resolve, reject) => {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString(); // Convert Buffer to string
+    });
+
+    req.on("end", () => {
+      resolve(JSON.parse(body)); // When all data has been received, resolve the promise
+    });
+
+    req.on("error", (err) => {
+      reject(err); // Handle errors
+    });
+  });
+};
+
+/* 
+@module merchantConfigHandler
+
+Handler function to handle the request
+ 
+*/
+
+const merchantConfigHandler = (
+  req,
+  res,
+  is_deployed = false,
+  domain = "default",
+  configPath = "dist/server/config/config.toml",
+) => {
+  let configFile = is_deployed ? configPath : "config/config.toml";
+  try {
+    getRequestBody(req).then((body) => {
+      Fs.readFile(configFile, { encoding: "utf8" }, (err, data) => {
+        if (err) {
+          errorHandler(res, "Error on Reading File");
+          return;
+        }
+        let config;
+        try {
+          config = toml.parse(data);
+        } catch {
+          errorHandler(res, "Error on Parsing toml");
+          return;
+        }
+        let merchantConfig = config["default"]["merchant_config"];
+        try {
+          let data = {};
+          // If the domain is present in the toml file
+          if (
+            domain.length > 0 &&
+            config[domain] != undefined &&
+            config[domain]["merchant_config"] != undefined &&
+            Object.keys(config[domain]["merchant_config"]).length > 0
+          ) {
+            data = updateMerchantConfigWithEnv(
+              config[domain]["merchant_config"],
+              body,
+              domain,
+            );
+          } else {
+            data = updateMerchantConfigWithEnv(merchantConfig, body, "default");
+          }
+          res.writeHead(200, {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          });
+          res.write(JSON.stringify(data));
+          res.end();
+        } catch (err) {
+          console.log(err, "Error on overriding merchant specific config");
+          errorHandler(res, "Error");
+          return;
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    errorHandler(res, "Server Error");
+  }
+};
+
+export { configHandler, merchantConfigHandler };
