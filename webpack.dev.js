122bf2ced56ea7266d149ab92c67ed4761e02f2b
Susritha Sabbini <susrithasabbini@gmail.com>
2025-02-22 00:51:56 +0530

    chore: Config and Webpack dev changes

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ let port = 9000;
 // proxy is setup to make frontend and backend url same for local testing
 let proxy = {
   "/api": {
-    target: "http://localhost:8000/api",
+    target: "http://localhost:8080",
     pathRewrite: { "^/api": "" },
     changeOrigin: true,
   },
