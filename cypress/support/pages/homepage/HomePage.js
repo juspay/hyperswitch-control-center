de6676e8e087ca3d80102884d16f3d6eba0e7ccb
prajwalnl0 <prajwal.nl@juspay.in>
2025-03-24 19:08:10 +0530

    test: added workflow test cases

---
+++ cypress/support/pages/homepage/HomePage.js
@@ -0,0 +1,50 @@
+class HomePage {
+  // Onboarding
+  get enterMerchantName() {
+    return cy.get('[name="merchant_name"]');
+  }
+
+  get onboardingSubmitButton() {
+    return cy.get('[data-button-for="startExploring"]');
+  }
+
+  get orgDropdown() {
+    return cy.get();
+  }
+
+  get merchantDropdown() {
+    return cy.get();
+  }
+
+  get profileDropdown() {
+    return cy.get('[class="md:max-w-40 max-w-16"]');
+  }
+
+  get profileDropdownList() {
+    return cy.get(
+      '[class="max-h-72 overflow-scroll px-1 pt-1 border border-b-0 sidebar-scrollbar"]',
+    );
+  }
+
+  //Sidebar
+
+  //Connectors
+  get connectors() {
+    return cy.get("[data-testid=connectors]");
+  }
+
+  get paymentProcessors() {
+    return cy.get("[data-testid=paymentprocessors]");
+  }
+
+  //Workflow
+  get workflow() {
+    return cy.get('[data-testid="workflow"]');
+  }
+
+  get routing() {
+    return cy.get('[data-testid="routing"]');
+  }
+}
+
+export default HomePage;
