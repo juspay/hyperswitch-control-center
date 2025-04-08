d16b31bec3f6d9d41e0469cdc5b4fac6aed2e9fc
“MuditBhatia12” <bhatia.mudit12@gmail.com>
2025-04-08 12:07:36 +0530

    fix: fixed vault 13 inch screen ui bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,7 +61,6 @@ module.exports = {
         "130-px": "130px",
         "195-px": "195px",
         "774-px": "774px",
-        "923-px": "923px",
         "12.5-rem": "12.5rem",
         onBordingSupplier: "calc(100vh - 300px)",
       },
