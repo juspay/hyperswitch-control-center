330830f352b9bda7c7141f6ddf03c124d13106d7
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2025-09-18 13:22:05 +0530

    fix: webpack failing

--- webpack.custom.js
+++ webpack.custom.js
@@ -9,7 +9,7 @@ const serverConfig = require("./webpack.server");
 let customBuild = () => {
   const isDevelopment = process.env.NODE_ENV !== "production";
   let entryObj = {
-    app: `./src/entryPoints/HyperSwitchEntry.res.js`,
+    app: `./src/entry-points/HyperSwitchEntry.res.js`,
   };
   return {
     mode: "production",
