9fd26473ff7ddc7a0f0d15779c2de3676306934c
Susritha <susrithasabbini@gmail.com>
2026-03-17 00:59:14 +0530

    fix: test cases

--- cypress/support/pages/auth/ResetPasswordPage.js
+++ cypress/support/pages/auth/ResetPasswordPage.js
@@ -4,7 +4,7 @@ class ResetPasswordPage {
   }
 
   get confirmPassword() {
-    return cy.get('[data-testid="confirm_password"]').children().eq(1);
+    return cy.get('[data-testid="confirm_password"] input').last();
   }
 
   get eyeIcon() {
