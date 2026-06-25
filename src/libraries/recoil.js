3950c685520d0f077b0a4e648081576d7da7a09e
Prajwal N L <prajwal.nl@juspay.in>
2026-06-25 20:37:50 +0530

    fix: remove unused imports from recoil helper

--- src/libraries/recoil.js
+++ src/libraries/recoil.js
@@ -1,10 +1,4 @@
-import {
-  RecoilRoot,
-  atom as recoilAtom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
+import { atom as recoilAtom } from "recoil";
 
 export function atom(key, defaultVal) {
   return recoilAtom({ key, default: defaultVal });
